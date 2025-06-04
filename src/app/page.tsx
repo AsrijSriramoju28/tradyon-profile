9dfff38c4b7cd1e87ee2eb6aad32e98f5b84533d
AsrijSriramoju28 <sriramojuasrij28@gmail.com>
2025-06-04 23:25:46 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import CompanyOverview from "../../profile-components/Company-Overview";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 p-6">
+      <CompanyOverview />
+    </main>
+  );
+}
